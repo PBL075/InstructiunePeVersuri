aa06b4413699bab116242480e79281f7ae52d486
PBL075 <pavelgorea75@gmail.com>
2026-05-19 16:17:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lucrul_cu_Versuri
+{
+    class Program
+    {
+        static void Main()
+        {
+            string word = "Hello";
+            word += "!";
+            //System.Console.WriteLine(word.Length);
+            //String.Concat(word, "!!");
+            //Console.WriteLine(String.Compare(word, "Hello!"));
+
+            //string people = "Alex,Bob,John";
+            //string[] names = people.Split(new char[]{','});
+            //foreach(string el in names)
+            //Console.WriteLine(el);
+
+            //people = String.Join(" ", names);
+            //System.Console.WriteLine(people);
+
+            //System.Console.WriteLine(word.Trim());
+
+            //System.Console.WriteLine(word.Substring(0, word.Length -1));
+
+            //System.Console.WriteLine(word.ToUpper());
+
+
+            
+        }  
+    }
+}
